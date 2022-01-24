deb5f9fb9af697e2f736242e89b4a67684ef9b64
Cataluna <catalunalilith2680@gmail.com>
2022-01-24 17:13:49 -0500

    outline priceUnderMax() body

--- PriceMonitorMain.py
+++ PriceMonitorMain.py
@@ -4,6 +4,8 @@ Created on Mon Jan 24 12:30:16 2022
 
 @author: catal
 """
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 """
 concept:
     bot that checks specified amazon webpage, and emails if price is bellow a set price
@@ -32,13 +34,18 @@ concept:
 """
 
 
-def priceUnderMax(amazonURL, max_price):
-    """assumes amazonURL is a string, representing an amazon item listing's URL
+def priceUnderMax(URL, max_price):
+    """assumes URL is a string, representing an amazon item listing's URL
     assumes max_price is a numerice, representing the price at which to alert
     returns a boolean if the price of the item is <= max_price
     """
-    pass
+    # get webpage
+    # find current_price
+    current_price = 0  # TODO
+    # return result
+    return current_price < max_price
 
 
 if __name__ == "__main__":
     print("kittens")
+    priceUnderMax("https://www.amazon.ca/gp/product/B00K0A37OG/", 99.99)
